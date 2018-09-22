30fb0749bd64c06497ec2319765a4e315e9dec76
Quinn <39351374+Quinn1876@users.noreply.github.com>
2018-09-22 18:00:48 -0400

    Add files via upload

--- Deck of Cards/card.py
+++ Deck of Cards/card.py
@@ -1,142 +1,182 @@
+################################################
+# @Author Quinn H
+# @FileName card.py
+# @Project DeckOfCards   
+#
+# @Version 1.1
+# @LastUpdated Sep. 22, 2018
+################################################
+
+
+
 import random
+
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
 class Card:
-    """
-    This is for a Generic Playing card
-    """
+    ####################################
+    # This is for a Generic Playing card
+    # Each card has a number and a suit
+    ####################################
     def __init__(self, suit, number):
-        """
-        Initializes a Card
-        """
+        # Initializes a Card
+        
         self.suit = suit
         self.number = number
         
     def getSuit(self):
-        """
-        returns the suit of the card
-        """
+       # Returns the suit of the card
+
         return self.suit
     
     def getNumber(self):
         return self.number
 
+    def getValue(self):
+        if self.number in [10,11,12,13]:
+            return 10
+        else:
+            return self.number
+
     def getColour(self):
+        ##################################################
+        # If the card is a Diamond or a Heart, return red
+        #
+        # If the card is a Spade or a Club, return black 
+        ##################################################
+
         if self.suit.startswith('D') or self.suit.startswith('H'):
             return 'red'
-        if self.suit.startswoth('C') or self.suit.startswith('S'):
+        if self.suit.startswith('C') or self.suit.startswith('S'):
             return 'black'
 
-    def printName(self):
+    def getName(self):
         if self.number == 1:
-            print('Ace of', self.suit)
+            return 'Ace of ' + self.suit
+        elif self.number == 11:
+            return 'Jack of ' + self.suit
+        elif self.number == 12:
+            return 'Queen of ' + self.suit
+        elif self.number == 13:
+            return 'King of ' + self.suit
         else:
-            print(self.number, 'of', self.suit)
+            return str(self.number) + ' of ' + self.suit
 
 
 
 class Deck:
-    """
-    Deck contains 52 card objects, 
-    can be shuffled 
-    item 0 in cards is top of deck
-    51 is bottom of deck when deck is full
-    """
+    ########################################
+    # Deck contains 52 card objects, 
+    # can be shuffled 
+    # item 0 in cards is top of deck
+    # 51 is bottom of deck when deck is full
+    ########################################
 
     
     def __init__(self):
-        """
-        Initializes a deck with 52 cards
-        Deck has a Main deck called cards &&
-        Deck has a Dicard Pile
-        """
+        #######################################
+        # Initializes a deck with 52 cards
+        # Deck has a Main deck called cards &&
+        # Deck has a Dicard Pile
+        #######################################
 
+        global suits 
+        
         self.cards = []
         self.discardPile = []
-        for n in range(12):#A generic deck has 12 cards with 4 suits. This creates each card
-            for s in suits:
-                c = Card(s, n + 1)
+        for number in range(12): # A generic deck has 12 cards with 4 suits. This creates each card
+            for suite in suits:
+                c = Card(suit, number + 1)
                 self.cards.append(c)
 
     
     def getDeck(self):
-        """
-        returns all cards in the deck in the form of a list
-        """
+        #######################################################
+        # returns all cards in the deck in the form of a list
+        #######################################################
         return self.cards
 
     
     def shuffle(self):
-        """
-        Shuffles the cards
-        """
+        #######################
+        # Shuffles the cards
+        #######################
         random.shuffle(self.cards)
 
     
     def addDiscard(self, card):
-        """
-        Adds a card to the discard pile
-        """
+        #################################
+        # Adds a card to the discard pile
+        #################################
         self.discardPile.append(card)
 
     
     def insertCard(self, card, spot = None):
-        """
-        Inserts the card into the deck
-        If a spot is not deermined in the call, it adds to a random spot
-        """
+        ##################################################################
+        # Inserts the card into the deck
+        # If a spot is not deermined in the call, it adds to a random spot
+        ##################################################################
         if spot is None:
             spot = random.randint(len(self.cards))
         self.cards.insert(spot, card)
 
     
     def getDeckSize(self):
-        """
-        returns the number of cards left in the deck
-        """
+        ##############################################
+        # Returns the number of cards left in the deck
+        ##############################################
         return len(self.cards)
 
     
     def deal(self, nhands, cph):
-        """
-        returns a list of lists
-        outerlist has 'nhands' items -- Number Of Hands
-        each inner list has 'cph' items -- Cards per Hand
-
-            ****
-            Currently Need to Add a Case for when the deck 
-            runs out of cards and there is no cards in the
-            discard pile
-            ****
-        """
+        #####################################################
+        # Returns a list of lists
+        # outerlist has 'nhands' items -- Number Of Hands
+        # each inner list has 'cph' items -- Cards per Hand
+        #
+        #    ****
+        #    Currently Need to Add a Case for when the deck 
+        #    runs out of cards and there is no cards in the
+        #    discard pile
+        #    ****
+        ######################################################
 
         hands = []
         for c in range(cph):
             hand = []
             for h in range(nhands):
+                
                 #Checks if deck has cards
                 try:
+                    
                     #if no more cards, raise exception
                     if len(self.cards) <= 0:
                         raise OutOfCards
+                    
                 #if out of cards, put cards from dicard pile back in deck and shuffle
                 except OutOfCards:
-                    print('You are out of cards, now shuffling in discard pile')
+                    print('You are out of cards, now shuffling in the discard pile')
                     self.cards = self.discardPile
                     self.discardPile = []
                     shuffle()
+                    
                 #deals a card to a hand and removes if from the deck
                 finally:
                     hand.append(self.cards[0])
                     self.cards.remove(self.cards[0])
+                    
             hands.append(hand)
+            
         return hands
 
 
 
                              
 class OutOfCards(Exception):
-    """
-    raised when the deck is out of cards
-    """
+    #######################################
+    # Raised when the deck is out of cards
+    #######################################
     pass
+
+
+
--- Deck of Cards/test.py
+++ Deck of Cards/test.py
@@ -1,12 +1,88 @@
+################################
+# @Author Quinn H
+# @FileName 
+# @Project DeckOfCards
+# 
+# @LastUpdated Spe. 22, 2018
+################################
 import card
+from card import Card, Deck
+import unittest
 
-deck = card.Deck()
-deck.shuffle()
-hands = deck.deal(2,6)
+#deck = Deck()
+#deck.shuffle()
+#hands = deck.deal(2,6)
 
-for hand in hands:
-    print('hand')
-    for card in hand:
-        card.printName()
-print('--------------------')
+#for hand in hands:
+#    print('hand')
+#    for card in hand:
+#        card.printName()
+#print('--------------------')
 
+class TestCardClass(unittest.TestCase):
+    card1 = Card(card.suits[0], 1)
+    card2 = Card(card.suits[3], 10)
+    card3 = Card(card.suits[2], 11)
+    card4 = Card(card.suits[1], 12)
+    card5 = Card(card.suits[1], 13)
+
+    def test_getName(self):
+        #######################################
+        # Tests the getName function to make 
+        # sure that all of the non number cards
+        # print properly.
+        #######################################
+        self.assertTrue(self.card1.getName() == "Ace of Diamonds", msg="Test 1 Fail")
+        self.assertTrue(self.card2.getName() == "10 of Spades", msg="Test 2 Fail")
+        self.assertTrue(self.card3.getName() == "Jack of Clubs", msg="Test 3 Fail")
+        self.assertTrue(self.card4.getName() == "Queen of Hearts", msg="Test 4 Fail")
+        self.assertTrue(self.card5.getName() == "King of Hearts", msg="Test 5 Fail")
+
+    def test_getColour(self):
+        #######################################
+        # Tests the getColour function to make 
+        # sure that all suits return properly.
+        #######################################
+
+        self.assertTrue(self.card1.getColour() == "red", msg="Test 1 Fail")
+        self.assertTrue(self.card2.getColour() == "black", msg="Test 2 Fail")
+        self.assertTrue(self.card3.getColour() == "black", msg="Test 3 Fail")
+        self.assertTrue(self.card4.getColour() == "red", msg="Test 4 Fail")
+
+    def test_getSuit(self):
+        #######################################
+        # Tests the getColour function to make 
+        # sure that all suits return properly.
+        #######################################
+
+        self.assertTrue(self.card1.getSuit() == "Diamonds", msg="Test 1 Fail")
+        self.assertTrue(self.card2.getSuit() == "Spades", msg="Test 2 Fail")
+        self.assertTrue(self.card3.getSuit() == "Clubs", msg="Test 3 Fail")
+        self.assertTrue(self.card4.getSuit() == "Hearts", msg="Test 4 Fail")
+
+    def test_getNumber(self):
+        ######################################
+        # Tests to make sure that the number 
+        # for the Ace Ten Jack Queen and King 
+        # returns 1    10  11   12       13
+        ######################################
+        self.assertTrue(self.card1.getNumber() == 1, msg="Test 1 Fail")
+        self.assertTrue(self.card2.getNumber() == 10, msg="Test 2 Fail")
+        self.assertTrue(self.card3.getNumber() == 11, msg="Test 3 Fail")
+        self.assertTrue(self.card4.getNumber() == 12, msg="Test 4 Fail")
+        self.assertTrue(self.card5.getNumber() == 13, msg="Test 5 Fail")
+
+    def test_getValue(self):
+        ######################################
+        # Tests to make sure that the number 
+        # for the Ace TEN Jack Queen and King 
+        # returns  1   10  10   10       10
+        ######################################
+        self.assertTrue(self.card1.getValue() == 1, msg="Test 1 Fail")
+        self.assertTrue(self.card2.getValue() == 10, msg="Test 2 Fail")
+        self.assertTrue(self.card3.getValue() == 10, msg="Test 3 Fail")
+        self.assertTrue(self.card4.getValue() == 10, msg="Test 4 Fail")
+        self.assertTrue(self.card5.getValue() == 10, msg="Test 5 Fail")
+
+if __name__ == '__main__':
+    unittest.main()
